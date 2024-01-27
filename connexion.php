34eface70fe47cbbab6e287aa27de13f2eb4ba75
ElianGARRIGUES <elian.garrigues@hotmail.fr>
2024-01-27 13:02:24 +0100

    inscription

---
+++ connexion.php
@@ -0,0 +1,37 @@
+<?php
+require_once "cruds/crud_users.php";
+require_once "db_connect.php";
+require_once "utils.php";
+
+if (update_post_var())
+{   
+    if (isset($_POST["username"]) && isset($_POST["mail"]) && isset($_POST["password"]))
+    {
+        // check if the user name is available
+        if(count(select_all_user_with_parameter($conn, "username", $username)) != 0)
+        {
+            return cant_be_used_data_error_message();
+        }
+        // check if the mail is available
+        if(count(select_all_user_with_parameter($conn, "mail", $mail)) != 0)
+        {
+            return cant_be_used_data_error_message();
+        }
+        $password_hashed = password_hash($password_raw, PASSWORD_DEFAULT);
+
+        // Set the final variables
+        $permission = 0;
+        // register the user in the db
+        $res = create_user($conn, $mail, $username, $password_hashed, $permission);
+        if ($res)
+        {
+            $res = success_message_json(201, "201 Created: New user successfully created");
+        }
+        else
+        {
+            $res = error_message_json(500, "500 Internal Server Error: Could not create the user");
+        }
+        return $res;
+    }
+
+}
---
+++ db_connect.php
@@ -0,0 +1,5 @@
+<?php
+$conn = mysqli_connect ("localhost" , "info9" , "U8H" , "info9" )
+;
+mysqli_set_charset($conn , "utf8" ) ;
+?>

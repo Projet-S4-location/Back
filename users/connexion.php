544f82764ea0920f2d7b06a0cc861e63cc32b61a
ElianGARRIGUES <elian.garrigues@hotmail.fr>
2024-02-23 20:41:44 +0100

    Update connexion.php

--- users/connexion.php
+++ users/connexion.php
@@ -12,67 +12,64 @@ ini_set('session.gc_maxlifetime', 86400);
 
 session_start();
 
-function login(){
-    if (update_post_var())
+if (update_post_var())
+{
+    // get the data
+    if (isset($_POST["username"]) && isset($_POST["password"]))
     {
-        // get the data
-        if (isset($_POST["username"]) && isset($_POST["password"]))
+
+
+        // sanitize the data
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        $conn = db_connect();
+        // search for username in users
+        $user = select_user_by_username($conn, $username);
+        // if not found
+        if(is_null($user))
         {
+            // We know that it's the username that is wrong
+            // But we say also wrong password to not give any
+            // information of who has an account on our website
+            echo error_message_json(401, "401 Unauthorized: Invalid username or password.");
+        }
 
-
-            // sanitize the data
-            $username = $_POST["username"];
-            $password = $_POST["password"];
-            $conn = db_connect();
-            // search for username in users
-            $user = select_user_by_username($conn, $username);
-            // if not found
-            if(is_null($user))
+        // if found and good password
+        if (password_verify($password, $user["password"]))
+        {
+            if (is_logged_in())
             {
-                // We know that it's the username that is wrong
-                // But we say also wrong password to not give any
-                // information of who has an account on our website
-                return error_message_json(401, "401 Unauthorized: Invalid username or password.");
+                session_unset();
             }
-
-            // if found and good password
-            if (password_verify($password, $user["password"]))
-            {
-                // should not happen but if the user was already connected
-                if (is_logged_in())
-                {
-                    session_unset();
-                }
-
-                // update the restriction
-                // set the session 
-                $_SESSION["id_user"] = $user["id_user"];
-                $_SESSION["username"] = $user["username"];
-                $_SESSION["permission"] = $user["permission"];
+            // should not happen but if the user was already connected
+            // update the restriction
+            // set the session 
+            $_SESSION["id_user"] = $user["id_user"];
+            $_SESSION["username"] = $user["username"];
+            $_SESSION["permission"] = $user["permission"];
                 
-                // echo 
-                $user_data = make_data_of_user($_SESSION);
+            // echo 
+            $user_data = make_data_of_user($_SESSION);
 
-                return json_encode($user_data);
-            }
-            else
-            {   
-                return error_message_json(401, "401 Unauthorized: Invalid username or password.");
-
-            }
-
+            echo json_encode($user_data);
         }
         else
-        {
-            return invalid_format_data_error_message();
+        {   
+            echo error_message_json(401, "401 Unauthorized: Invalid username or password.");
+
         }
+
     }
     else
     {
-        return no_data_error_message();
+        echo invalid_format_data_error_message();
     }
-    
 }
+else
+{
+    echo no_data_error_message();
+}
+    
 
 function make_data_of_user($tab){
     $data = [
@@ -83,4 +80,3 @@ function make_data_of_user($tab){
     return $data;
 }
 
-print(login());
